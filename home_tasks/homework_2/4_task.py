a0baf663d3b91ffb4741162795064a1f8b99d106
maksim <maximgekalo@gmail.com>
2021-06-25 16:10:22 +0300

    HW2: task 4
    * I did task 4 from the list of tasks

---
+++ home_tasks/homework_2/4_task.py
@@ -0,0 +1,6 @@
+vegetarians = ['Maksim Gekalo', 'Alex Wallace', 'Anthony Labert','Thomas Harris']
+omnivores = ['Tyler Beverly', 'William Ford', 'Brandon Newman', 'George Allen']
+vegetarians.extend(omnivores)
+
+for x in vegetarians:
+        print(f'{x}, eats vegetables and herbs')
