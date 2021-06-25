2267f14910797afdb23e9c099d40a34585188124
maksim <maximgekalo@gmail.com>
2021-06-25 16:10:37 +0300

    HW2: task 5
    * I did task 5 from the list of tasks

---
+++ home_tasks/homework_2/5_task.py
@@ -0,0 +1,50 @@
+vip_plases = {
+    1: 'Maksim',
+    2: 'Alex',
+    3: 'Anthony',
+    4: 'Thomas'
+}
+
+hall_plases = {
+    1: 'Alex',
+    2: 'George',
+    3: 'Oscar',
+    4: 'George',
+    5: 'Free',
+    6: 'Free',
+    7: 'Free',
+}
+
+
+vip_lodge = (vip_plases[1], vip_plases[2], vip_plases[3], vip_plases[4])
+
+common_hall = [hall_plases[1], hall_plases[2], hall_plases[3], hall_plases[4]]
+
+
+
+
+
+
+# demonstration
+def add_person(take_quantity) -> int:
+    name = 0
+    take_quantity = int(input('specify no more than 3 persons :'))
+    for x in range(take_quantity):
+        name = input('enter the name of the guest :')
+        for n in hall_plases:
+            if hall_plases[n] == 'Free':
+                hall_plases[n] = name
+                print("one guest was added")
+                break
+        common_hall.append(hall_plases[5])
+    print(common_hall)
+    print(hall_plases)
+
+add_person(3)
+
+
+
+
+
+
+
