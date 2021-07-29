34d6b8806b2e532da13a30046f7d9ab2737a0d27
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-29 15:15:18 +0300

    HW9: add described issues in task1 todo file

--- home_tasks/homework_9/1_task(arithmetic_package)/arithmetic_module.py
+++ home_tasks/homework_9/1_task(arithmetic_package)/arithmetic_module.py
@@ -1,16 +1,10 @@
-
-
 def addition(first_operand: int, second_operand: int) -> int:
     return first_operand + second_operand
+
 
 def subtraction(first_operand: int, second_operand: int) -> int:
     return first_operand - second_operand
 
+
 def integer_division(first_operand: int, second_operand: int) -> int:
     return first_operand // second_operand
-
-
-if __name__ == '__main__':
-    print(addition(99, 1))
-    print(subtraction(100, 1))
-    print(integer_division(100, 10))
